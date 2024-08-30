92165337a9221754d6dcae12550e19ce33d35525
blukai <20866892+blukai@users.noreply.github.com>
2024-08-30 16:25:28 +0200

    char cursor

---
+++ crates/dungers_charsor/src/lib.rs
@@ -0,0 +1,157 @@
+use core::ops::Range;
+
+/// enhanced version of [std::str::Chars].
+#[derive(Debug, Clone)]
+pub struct CharCursor<'a> {
+    input: &'a str,
+    /// byte position relative to the beginning of the input.
+    offset: usize,
+}
+
+impl<'a> CharCursor<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Self { input, offset: 0 }
+    }
+
+    /// peek returns but does not consume the next char in the input.
+    #[must_use]
+    #[inline]
+    pub fn peek(&self) -> Option<char> {
+        self.input[self.offset..].chars().next()
+    }
+
+    /// next advances the offset and returns the next char.
+    #[must_use]
+    #[inline]
+    pub fn next(&mut self) -> Option<char> {
+        self.peek().map(|ch| {
+            self.offset += ch.len_utf8();
+            ch
+        })
+    }
+
+    /// prev returns the previous char in the input, without modifying the offset.
+    #[must_use]
+    #[inline]
+    pub fn prev(&self) -> Option<char> {
+        self.input[..self.offset].chars().next_back()
+    }
+
+    /// eat_while advances the offset and skips over chars while the given closure returns true.
+    /// returns the number of chars skipped.
+    pub fn eat_while(&mut self, func: impl Fn(char) -> bool) -> usize {
+        let mut n = 0;
+        while let Some(ch) = self.peek() {
+            if !func(ch) {
+                break;
+            }
+            n += 1;
+            self.offset += ch.len_utf8();
+        }
+        n
+    }
+
+    /// returns the byte position of the next char, relative to the beginning of the input, or the
+    /// length of the underlying str if there are no more characters.
+    #[must_use]
+    #[inline]
+    pub fn offset(&self) -> usize {
+        self.offset
+    }
+
+    /// returns the byte position of the prev char, relative to the beginning of the input.
+    #[must_use]
+    #[inline]
+    pub fn prev_offset(&self) -> usize {
+        self.offset - self.prev().map_or(0, |ch| ch.len_utf8())
+    }
+
+    /// returns a slice of the input str at the specified byte range. panics if the range extends
+    /// beyond the length of the input str.
+    #[must_use]
+    #[inline]
+    pub fn slice_range(&self, range: Range<usize>) -> &'a str {
+        &self.input[range]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use lazy_static::lazy_static;
+    use static_assertions::const_assert;
+
+    use super::*;
+
+    const INPUT: &str = "こんにちは";
+    const_assert!(INPUT.len() > 2);
+    lazy_static! {
+        static ref INPUT_CHARS: Vec<char> = INPUT.chars().collect();
+    }
+
+    #[test]
+    fn test_peek() {
+        let mut cc = CharCursor::new(INPUT);
+        assert_eq!(cc.peek(), Some(INPUT_CHARS[0]));
+        assert_eq!(cc.next(), Some(INPUT_CHARS[0]));
+    }
+
+    #[test]
+    fn test_next() {
+        let mut cc = CharCursor::new(INPUT);
+        for ch in INPUT_CHARS.iter().cloned() {
+            assert_eq!(cc.next(), Some(ch));
+        }
+    }
+
+    #[test]
+    fn test_prev() {
+        let mut cc = CharCursor::new(INPUT);
+        assert_eq!(cc.prev(), None);
+        assert_eq!(cc.next(), Some(INPUT_CHARS[0]));
+        assert_eq!(cc.prev(), Some(INPUT_CHARS[0]));
+    }
+
+    #[test]
+    fn test_eat_while() {
+        const PREFIX: &str = "    ";
+        assert!(PREFIX.len() > 0);
+
+        let input = format!("{PREFIX}{INPUT}");
+
+        let mut cc = CharCursor::new(&input);
+        cc.eat_while(|ch| ch.is_whitespace());
+        assert_eq!(cc.offset(), PREFIX.len());
+    }
+
+    #[test]
+    fn test_offset() {
+        let mut cc = CharCursor::new(INPUT);
+        let _ = cc.next();
+        assert_eq!(cc.offset(), INPUT_CHARS[0].len_utf8());
+        let _ = cc.next();
+        assert_eq!(
+            cc.offset(),
+            INPUT_CHARS[0].len_utf8() + INPUT_CHARS[1].len_utf8()
+        );
+    }
+
+    #[test]
+    fn test_prev_offset() {
+        let mut cc = CharCursor::new(INPUT);
+        assert_eq!(cc.prev_offset(), 0);
+        let _ = cc.next();
+        assert_eq!(cc.prev_offset(), 0);
+        let _ = cc.next();
+        assert_eq!(cc.prev_offset(), INPUT_CHARS[0].len_utf8());
+    }
+
+    #[test]
+    fn test_slice_range() {
+        let cc = CharCursor::new(INPUT);
+        let range = Range {
+            start: 0,
+            end: INPUT_CHARS[0].len_utf8(),
+        };
+        assert_eq!(cc.slice_range(range), INPUT_CHARS[0].to_string());
+    }
+}
