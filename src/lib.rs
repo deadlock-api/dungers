5cae4b25f8ada2ba461a9740ee4e8d7fff67df6a
blukai <20866892+blukai@users.noreply.github.com>
2024-09-03 00:05:54 +0200

    introduce root crate with feature flags that re-exports other crates

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#[cfg(feature = "bitbuf")]
+pub use dungers_bitbuf as bitbuf;
+
+#[cfg(feature = "charsor")]
+pub use dungers_charsor as charsor;
+
+#[cfg(feature = "genvec")]
+pub use dungers_genvec as genvec;
+
+#[cfg(feature = "varint")]
+pub use dungers_varint as varint;
