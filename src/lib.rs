788a953403ae4c640fed1a62a0728bca1f4218d2
Manuel Raimann <raimannma@outlook.de>
2025-07-16 20:22:31 +0200

    Remove rangealloc and genvec and benchmarks

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,6 @@
 #[cfg(feature = "bitbuf")]
 pub use dungers_bitbuf as bitbuf;
-
 #[cfg(feature = "charsor")]
 pub use dungers_charsor as charsor;
-
-#[cfg(feature = "genvec")]
-pub use dungers_genvec as genvec;
-
-#[cfg(feature = "rangealloc")]
-pub use dungers_rangealloc as rangealloc;
-
 #[cfg(feature = "varint")]
 pub use dungers_varint as varint;
